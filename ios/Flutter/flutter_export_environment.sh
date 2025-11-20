8a269bc030f68b05cfc37bdf9a60639ad9c58e46
voda-bizzdom <david@bizzdom.com>
2025-11-20 12:32:20 +0100

    Add ios/Flutter/flutter_export_environment.sh

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/david/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/david/Projects/cc-firstbizzdom-app/loyalty_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.7"
+export "FLUTTER_BUILD_NUMBER=8"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/david/Projects/cc-firstbizzdom-app/loyalty_app/.dart_tool/package_config.json"
